d826ccf50c67020b72183e7bffc334a757b51455
Ryan Araujo <ryanaraujoxd@gmail.com>
2024-07-07 00:45:18 -0300

    aula sobre variaveis

---
+++ aula 05/javascript/index.js
@@ -0,0 +1,20 @@
+// Nao podemos criar variaveis com palavras reservadas.
+// Variaveis precisam ter nomes significativos
+
+let nome = 'Ryan'; // Variavel
+
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Emanuelle.');
+console.log(nome, 'casou-se com Emanuelle em 2012.');
+
+console.log('Emanuelle teve 1 filho com', nome, 'em 2015.');
+console.log('A filha de' ,nome, 'se chama Lilith.');
+
+
+// nao pode comecar o nome de uma variavel com numero
+// nao pode conter espacos ou tracos
+// ut ilizamos camelCase
+// Case-sensitive
+// Nao podemos redeclarar variaveis com let
+// NAO UTILIZER VAR, UTILIZE LET.
