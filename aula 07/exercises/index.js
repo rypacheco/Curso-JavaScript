505876fa9c6453c23cce38665e8a80ed438e87b4
Ryan Araujo <ryanaraujoxd@gmail.com>
2024-07-10 23:57:23 -0300

    aula de exercicio sobre let e const.

---
+++ aula 07/exercises/index.js
@@ -0,0 +1,25 @@
+/* Haru tem 23 anos, pesa 84kg 
+tem 1.8 de altura e seu IMC e de 25.929292929292
+Haru Misaki nasceu em 2001 */
+
+const nome = 'Haru';
+const sobrenome = 'Misaki'; //string => ''
+const idade = 23; //number => 23
+const peso = 84;
+const alturaEmM = 1.80;
+
+let indiceDeMassaCorporal; // peso / (altura * altura)
+let anoNascimento
+
+indiceDeMassaCorporal = peso / (alturaEmM * alturaEmM);
+anoNascimento = 2024 - idade;
+
+
+// + unir valores 
+// $ {} colocar as variaveis dentro das chaves para mostrar o valor 
+// template strings 
+
+
+console.log(nome + ' ' + sobrenome + ' tem ' + idade + ' anos, pesa ' + peso + ' kg ');
+console.log(`tem ${alturaEmM}, de altura e seu IMC e de ${indiceDeMassaCorporal}`);
+console.log(`${nome} nasceu em ${anoNascimento}`);
