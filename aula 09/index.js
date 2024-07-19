0a25519ab565fdd17c932b2513d70a37c46d6a26
Ryan Araujo <ryanaraujoxd@gmail.com>
2024-07-19 00:33:50 -0300

    aula sobre tipos de dados primitivos.

---
+++ aula 09/index.js
@@ -0,0 +1,18 @@
+const nome = 'Haru'; // string 
+const nome1 = "Haru"; // string
+const nome2 = `Haru`; // string
+const num1 = 10; // number
+const num2 = 10.52; //number
+
+let nomeAluno; // undefined = nao aponta para lugar nenhum na memoria
+const sobrenomeAluno = null; // valor Nulo -> nao aponta para local nenhum na memoria
+const aprovado = false; // boolan = true, false (logico)
+
+
+console.log(typeof num1, num1);
+console.log(typeof num2, nome2);
+console.log(typeof nome, num1);
+console.log(typeof nome1, nome1);
+
+console.log(typeof aprovado, aprovado);
+
